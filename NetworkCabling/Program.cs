b4e4ed322a3c03c8fb732149f544f3891646274a
asseniliev <assen.s.iliev@gmail.com>
2023-04-29 22:30:09 +0200

    - Network cabling solution

---
+++ NetworkCabling/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * https://www.codingame.com/ide/puzzle/network-cabling
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        long cableLength;
+        int minX = int.MaxValue;
+        int minY = int.MaxValue;
+        int maxX = int.MinValue;
+        int maxY = int.MinValue;
+
+        int[] yLocations = new int[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int X = int.Parse(inputs[0]);
+            int Y = int.Parse(inputs[1]);
+
+            yLocations[i] = Y;
+
+            if (X > maxX) maxX = X;
+            if (Y > maxY) maxY = Y;
+
+            if (X < minX) minX = X;
+            if (Y < minY) minY = Y;
+        }
+
+        // Write an answer using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        cableLength = maxX - minX;
+        int median = medianValue(yLocations);
+
+        for (int house = 0; house < N; house++)
+        {
+            cableLength += Math.Abs(yLocations[house] - median);
+        }
+
+        Console.WriteLine(cableLength);
+    }
+
+    private static int medianValue(int[] arr)
+    {
+        int len = arr.Length;
+        Array.Sort(arr);
+
+        int medianIndex = len / 2;
+        int median;
+
+        if (len % 2 == 0)
+        { // array length is even
+            median = (arr[medianIndex - 1] + arr[medianIndex]) / 2;
+        }
+        else
+        { // array length is odd
+            median = arr[medianIndex];
+        }
+
+        return median;
+    }
+}
---
+++ ShadowsOfTheNight/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * https://www.codingame.com/ide/puzzle/shadows-of-the-knight-episode-1
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+        int W = int.Parse(inputs[0]); // width of the building.
+        int H = int.Parse(inputs[1]); // height of the building.
+        int N = int.Parse(Console.ReadLine()); // maximum number of turns before game over.
+        inputs = Console.ReadLine().Split(' ');
+        int X0 = int.Parse(inputs[0]);
+        int Y0 = int.Parse(inputs[1]);
+
+        int winX = X0;
+        int winY = Y0;
+
+        int xLeftBoundary = 0;
+        int xRightBoundary = W;
+        int yUpBoundary = 0;
+        int yDownBoundary = H;
+        
+
+        // game loop
+        while (true)
+        {
+            string bombDir = Console.ReadLine(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+
+            foreach (char letter in bombDir)
+            {
+                switch(letter)
+                {
+                    case 'U':
+                        yDownBoundary = winY;
+                        winY = (winY + yUpBoundary) / 2;                        
+                        break;
+                    case 'D':
+                        yUpBoundary = winY;
+                        winY = (winY + yDownBoundary) / 2;
+                        break;
+                    case 'L':
+                        xRightBoundary = winX;
+                        winX = (winX + xLeftBoundary) / 2;
+                        break;
+                    case 'R':
+                        xLeftBoundary = winX;
+                        winX = (winX + xRightBoundary) / 2;
+                        break;
+                }
+            }
+
+            N--;
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+
+            // the location of the next window Batman should jump to.
+            Console.WriteLine($"{winX} {winY}");
+        }
+    }
+}
